70dedeb9a1d712ee2d9c32eab44e756d859b734b
Boco14 <marbok836@gmail.com>
2022-11-06 03:28:46 +0800

    update5

--- Thesis-main/Thesis_Schedule/Thesis_Schedule_App/views.py
+++ Thesis-main/Thesis_Schedule/Thesis_Schedule_App/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hi(request):
-    return render(request,'Thesis_Schedule_App/loginpage.html')
+    return render(request,'Thesis_Schedule_App/admin_landingpage.html')
