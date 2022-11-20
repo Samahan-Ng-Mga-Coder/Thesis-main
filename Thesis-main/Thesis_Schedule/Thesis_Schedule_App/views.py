ff8881d20339196f8ff91085298bba0b30df5d84
Valenzuela, Nathaniel Rosell <99592583+SinOfGuardian@users.noreply.github.com>
2022-11-20 16:36:03 +0800

    error 02

--- Thesis-main/Thesis_Schedule/Thesis_Schedule_App/views.py
+++ Thesis-main/Thesis_Schedule/Thesis_Schedule_App/views.py
@@ -7,30 +7,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-<<<<<<< Updated upstream
-# def login(request):
-#
-#     if request.method == "POST":
-#         #request method
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         #check user request to authenticate
-#         user = authenticate(request, username=username, password=password)
-#
-#         #conditional statement
-#         if user is not None:
-#             login(request, user)
-#             return redirect(request, 'admin_landingpage')
-#         else:
-#             messages.error(request, "There Was an Error Logging In, Try Again...")
-#             return render(request, 'Registration/login_page.html')
-#
-#     else:
-#         return render(request, 'Registration/login_page.html')
 
-
-=======
->>>>>>> Stashed changes
 def adminlogin(request):
     if request.method == "POST":
         username = request.POST['username']
