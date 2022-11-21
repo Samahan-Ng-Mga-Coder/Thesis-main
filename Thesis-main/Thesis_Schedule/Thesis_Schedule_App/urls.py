d92bcef8f8fd0b906fb251a0324cf5bca3454a57
Valenzuela, Nathaniel Rosell <99592583+SinOfGuardian@users.noreply.github.com>
2022-11-21 09:41:36 +0800

    hahahahah

--- Thesis-main/Thesis_Schedule/Thesis_Schedule_App/urls.py
+++ Thesis-main/Thesis_Schedule/Thesis_Schedule_App/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.adminlogin, name='login_page'),
     path('admin_landingpage', views.adminlogin, name='admin_landingpage'),
     path('facultyloading', views.facultyloading, name='facultyloading'),
+    path('classschedule', views.classschedule, name='classschedule'),
 
     #path('Userinfo/', views.adminAddingUser, name='Userinfo'),
 
